85276b370140f477c9899f9e1834e46aa0e9405c
Steve Huff <steve.huff@runkeeper.com>
2017-05-18 13:27:03 -0400

    first try at elasticsearch module

--- main.tf
+++ main.tf
@@ -1,29 +1,50 @@
 # Elasticsearch domain
+data "aws_iam_policy_document" "es_management_instance_access" {
+  statement {
+    actions = [
+      "es:*",
+    ]
+
+    resources = [
+      "${aws_elasticsearch_domain.es.arn}",
+    ]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+
+      values = ["${distinct(compact(var.management_public_ip_addresses))}"]
+    }
+  }
+}
+
 resource "aws_elasticsearch_domain" "es" {
-  domain_name = "tf-${var.domain_name}"
+  domain_name           = "tf-${var.domain_name}"
   elasticsearch_version = "${var.es_version}"
+
   cluster_config {
-    instance_type = "${var.instance_type}"
-    instance_count = "${var.instance_count}"
+    instance_type            = "${var.instance_type}"
+    instance_count           = "${var.instance_count}"
     dedicated_master_enabled = "${var.instance_count >= 10 ? true : false}"
-    dedicated_master_count = 3
-    dedicated_master_type = "${var.dedicated_master_type ? var.dedicated_master_type : var.instance_type}"
+    dedicated_master_count   = 3
+    dedicated_master_type    = "${var.dedicated_master_type ? var.dedicated_master_type : var.instance_type}"
+    zone_awareness_enabled   = "${var.zone_awareness}"
   }
 
-  advanced_options {
+  # advanced_options {
+  # }
+
+  ebs_options {
+    ebs_enabled = "${var.ebs_volume_size > 0 ? true : false}"
+    volume_size = "${var.ebs_volume_size}"
+    volume_type = "${var.ebs_volume_type}"
   }
-
-  access_policies = 
-
   snapshot_options {
+    automated_snapshot_start_hour = "${var.snapshot_start_hour}"
   }
-
   tags {
     Domain = "${var.domain_name}"
   }
-
 }
 
-# Management instance with Elastic IP
-
-/* vim: set et fenc= ff=unix ft=terraform sts=2 sw=2 ts=2 : */
+# vim: set et fenc= ff=unix ft=terraform sts=2 sw=2 ts=2 : 
--- variables.tf
+++ variables.tf
@@ -1,26 +1,52 @@
 variable "domain_name" {
   description = "Domain name for Elasticsearch cluster (will be prefixed with 'tf-')"
-  default = "es-domain"
+  default     = "es-domain"
 }
 
 variable "es_version" {
   description = "Version of Elasticsearch to deploy (default 5.1)"
-  default = "5.1"
+  default     = "5.1"
 }
 
 variable "instance_type" {
   description = "ES instance type for data nodes in the cluster (default t2.small.elasticsearch)"
-  default = "t2.small.elasticsearch"
+  default     = "t2.small.elasticsearch"
 }
 
 variable "instance_count" {
   description = "Number of data nodes in the cluster (default 3)"
-  default = 6
+  default     = 6
 }
 
 variable "dedicated_master_type" {
   description = "ES instance type to be used for dedicated masters (default same as instance_type)"
-  default = false
+  default     = false
 }
 
-/* vim: set et fenc=utf-8 ff=unix ft=terraform sts=2 sw=2 ts=2 : */
+variable "management_public_ip_addresses" {
+  description = "List of IP addresses from which to permit management traffic"
+  type        = "list"
+  default     = []
+}
+
+variable "es_zone_awareness" {
+  description = "Enable zone awareness for Elasticsearch cluster (default false)"
+  default     = "false"
+}
+
+variable "ebs_volume_size" {
+  description = "Optionally use EBS volumes for data storage by specifying volume size in GB (default 0)"
+  default     = 0
+}
+
+variable "ebs_volume_type" {
+  description = "Storage type of EBS volumes, if used (default gp2)"
+  default     = "gp2"
+}
+
+variable "snapshot_start_hour" {
+  description = "Hour at which automated snapshots are taken, in UTC (default 0)"
+  default     = 0
+}
+
+# vim: set et fenc=utf-8 ff=unix ft=terraform sts=2 sw=2 ts=2 : 
