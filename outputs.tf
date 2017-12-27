479a40f5ef45b4258aeb161fdfecf2419ebd54a2
Alex <aliaksandr.hramovich@seavus.com>
2017-12-27 17:14:07 +0300

    Output interpolated local variables

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "arn" {
   description = "Amazon Resource Name (ARN) of the domain"
-  value       = "${aws_elasticsearch_domain.es.arn}"
+  value       = "${local.es_arn}"
 }
 
 output "domain_id" {
   description = "Unique identifier for the domain"
-  value       = "${aws_elasticsearch_domain.es.domain_id}"
+  value       = "${local.es_domain_id}"
 }
 
 output "endpoint" {
   description = "Domain-specific endpoint used to submit index, search, and data upload requests"
-  value       = "${aws_elasticsearch_domain.es.endpoint}"
+  value       = "${local.es_endpoint}"
 }
 
 /* vim: set et fenc= ff=unix ft=terraform sts=2 sw=2 ts=2 : */
