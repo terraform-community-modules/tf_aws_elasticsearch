f636382166d1d98248adf0ce380fa4c910020fc8
Danny Brody <dbrody@gofundme.com>
2018-05-01 13:45:42 -0700

    adding option to have encryption at rest inside a vpc

--- main_vpc.tf
+++ main_vpc.tf
@@ -36,6 +36,10 @@ resource "aws_elasticsearch_domain" "es_vpc" {
   domain_name           = "tf-${var.domain_name}"
   elasticsearch_version = "${var.es_version}"
 
+  encrypt_at_rest       = {
+    enabled = "${var.encrypt_at_rest}"
+  }
+  
   cluster_config {
     instance_type            = "${var.instance_type}"
     instance_count           = "${var.instance_count}"
