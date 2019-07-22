02d244c4dbd2893072e36ad2a5ba1a0a1e1b6130
root <root@localhost.localdomain>
2019-07-22 17:50:42 -0400

    upgraded to v 0.12

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
 locals {
-  domain_name = "${var.use_prefix ? join("", list(var.domain_prefix, var.domain_name)) : var.domain_name}"
-  inside_vpc  = "${length(var.vpc_options["subnet_ids"]) > 0 ? 1 : 0}"
+  domain_name = var.use_prefix ? join("", [var.domain_prefix, var.domain_name]) : var.domain_name
+  inside_vpc  = length(var.vpc_options["subnet_ids"]) > 0 ? true : false
 }
